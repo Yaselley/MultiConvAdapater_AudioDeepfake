d4f96ff1050fc04855151ff2b92f089adc49bf8f
Yaselley <elkheiryassine0@gmail.com>
2025-10-29 14:16:03 +0100

    add protocols

--- config.py
+++ config.py
@@ -5,30 +5,30 @@ import os
 ASV19 = {
     "train_flac": "/ds/audio/LA_19/ASVspoof2019_LA_train/flac/",
     "dev_flac": "/ds/audio/LA_19/ASVspoof2019_LA_dev/flac/",
-    "train_protocol": "/netscratch/yelkheir/datasets/asvsoof/train.txt",
-    "dev_protocol": "/netscratch/yelkheir/datasets/asvsoof/dev.txt"
+    "train_protocol": "./protocols/ASV19/train.txt",
+    "dev_protocol": "./protocols/ASV19/dev.txt"
 }
 
 # ---------------------- ASV5 ---------------------- #
 ASV5 = {
     "train_flac": "/ds-slt/audio/ASVSpoof2024/flac_T/",
     "dev_flac": "/ds-slt/audio/ASVSpoof2024/flac_D/",
-    "train_protocol": "/netscratch/yelkheir/datasets/asv5/asvspoof5.train.txt",
-    "dev_protocol": "/netscratch/yelkheir/datasets/asv5/asvspoof5.dev.{seed}.txt"
+    "train_protocol": "./protocols/ASV5/asvspoof5.train.txt",
+    "dev_protocol": "./protocols/ASV5/asvspoof5.dev.txt"
 }
 
 # ---------------------- ASV5 ---------------------- #
 TEST = {
     "flac": "/ds/audio/LA_19/ASVspoof2019_LA_train/flac/",
-    "protocol": "/netscratch/yelkheir/datasets/asvsoof/train.txt",
+    "protocol": "./protocols/ASV19/train.txt",
     "ext": "flac"
 }
 
 # ---------------------- Default settings ---------------------- #
 DEFAULT_MAX_LEN = 64600
 SAMPLE_RATE = 16000
-W2V_XLSR_CONFIG = "/netscratch/yelkheir/DeepFense/MultiConvAdapter/protocols/w2v_config.json"
-W2V_XLSR_CKPT = "/netscratch/yelkheir/ssl_models/xlsr2_300m.pt"
+W2V_XLSR_CONFIG = "./protocols/w2v_config.json"
+W2V_XLSR_CKPT = "./ssl_models/xlsr2_300m.pt"
 
 DEVICE = "cuda"
 BATCH_SIZE = 128
@@ -37,8 +37,8 @@ PIN_MEMORY = True
 SHUFFLE = False
 
 # ---------------------- Default Output Folder ---------------------- #
-DEFAULT_OUT_DIR = "/netscratch/yelkheir/standard/outputs/"
+DEFAULT_OUT_DIR = "./outputs/"
 
 # ---------------------- BEST EPOCH ---------------------- #
-BEST_CKPT = "/netscratch/yelkheir/DeepFense/MultiConvAdapter/protocols/CKPT/ASV19/SSL_best.pt"
-BEST_CONFIG = "/netscratch/yelkheir/DeepFense/MultiConvAdapter/protocols/CKPT/ASV19/config.json"
+BEST_CKPT = "./protocols/CKPT/ASV19/SSL_best.pt"
+BEST_CONFIG = "./protocols/CKPT/ASV19/config.json"
